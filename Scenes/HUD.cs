bd5652290076fbc4ec155a94c40e4fb908303b7b
phopps <pathopps1@gmail.com>
2023-03-29 21:08:14 -0400

    starting camara shake implementation for main cam using random noise values for camera offset

--- Scenes/HUD.cs
+++ Scenes/HUD.cs
@@ -6,6 +6,8 @@ public class HUD : CanvasLayer {
   [Export] public NodePath statsTextBox;
   private Label stb;
   [Signal] delegate void TestButtonPressed();
+  // [Export] public NodePath cameraPath;
+  // private MainCam camera;
 
   public override void _Ready() {
     stb = (Label) GetNodeOrNull(statsTextBox);
@@ -21,8 +23,10 @@ public class HUD : CanvasLayer {
   public void _on_TestButton_pressed() {
     GD.Print("Pressed test button!");
     EmitSignal("TestButtonPressed");
+    // camera = GetParent().GetNodeOrNull<MainCam>("MainCam");
+    // camera.Shake();
+    // TODO: screen shake, dust cloud particle emitter, thud sound effect
   }
-
 }
 
 
